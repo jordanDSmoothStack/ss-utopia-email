e39a9fbb1300efa993f3aacc6879c6b47de1e23e
Jordan Divina <jed6sn@virginia.edu>
2021-02-23 21:07:15 -0500

    Changed from using javax to using the AWS SDK

--- src/main/java/ss/utopia/email/service/MailServiceImpl.java
+++ src/main/java/ss/utopia/email/service/MailServiceImpl.java
@@ -1,15 +1,19 @@
 package ss.utopia.email.service;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 
-import javax.mail.Message.RecipientType;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ss.utopia.email.dto.EmailDto;
@@ -19,8 +23,7 @@ import ss.utopia.email.entity.MailEntity;
 @Service
 public class MailServiceImpl implements MailService {
 
-  static final String SMTPUsername = "AKIA5REGZ56II7SJYCVK";
-  static final String SMTPPassword = "BCS3eyH+7d4uQDLpX8HYPbPVj3brBddQfE3dJeANU06O";
+  static final EnvironmentVariableCredentialsProvider AWSCredentials = new EnvironmentVariableCredentialsProvider();
   static final String from = "jordan.divina@smoothstack.com";
   static final String fromName = "Utopia Airlines";
 
@@ -37,45 +40,35 @@ public class MailServiceImpl implements MailService {
     mailToBeSent.setSubject(subject);
     mailToBeSent.setHTMLBody(body);
 
-    try
-    {
+    try {
       return sendMailWithAWS(mailToBeSent);
-    }
-      catch (UnsupportedEncodingException | MessagingException e)
-    {
-      log.error(e.getMessage());
-      return ResponseEntity.badRequest().body("Email was not sent.");
+    } catch (IOException e) {
+      e.printStackTrace();
+      return ResponseEntity.badRequest().body("Email was not properly formatted");
     }
   }
 
-  private ResponseEntity<String> sendMailWithAWS(MailEntity mail) throws UnsupportedEncodingException, MessagingException {
-    final String host = "email-smtp.us-east-1.amazonaws.com";
-    final Integer port = 587;
-
-    Properties props = System.getProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.port", port);
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.smtp.auth", "true");
-
-    Session session = Session.getDefaultInstance(props);
-
-    MimeMessage msg = new MimeMessage(session);
-    msg.setFrom(new InternetAddress(mail.getFrom(), mail.getFromName()));
-    msg.setRecipient(RecipientType.TO, new InternetAddress(mail.getTo()));
-    msg.setSubject(mail.getSubject());
-    msg.setContent(mail.getHTMLBody(), "text/html");
-
-    try (Transport transport = session.getTransport()) {
-        log.info("Sending a message to: " + mail.getTo());
-        transport.connect(host, SMTPUsername, SMTPPassword);
-        transport.sendMessage(msg, msg.getAllRecipients());
-        log.info("Message was successfully created");
-        return ResponseEntity.ok("Email was successfully sent through SES");
+  private ResponseEntity<String> sendMailWithAWS(MailEntity mail) throws IOException {
+    try {
+      AmazonSimpleEmailService client =
+          AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(
+              new AWSStaticCredentialsProvider(AWSCredentials.getCredentials())).withRegion(Regions.US_EAST_1).build();
+      SendEmailRequest request = new SendEmailRequest()
+          .withDestination(
+              new Destination().withToAddresses(mail.getTo()))
+          .withMessage(new Message()
+              .withBody(new Body()
+                  .withHtml(new Content()
+                      .withCharset("UTF-8").withData(mail.getHTMLBody())))
+              .withSubject(new Content()
+                  .withCharset("UTF-8").withData(mail.getSubject())))
+          .withSource(mail.getFrom());
+      client.sendEmail(request);
+      log.info("Email sent to " + mail.getTo());
+      return ResponseEntity.ok("Email was sent to " + mail.getTo());
     } catch (Exception ex) {
-        log.error("Message was not sent");
-        log.error("Error message: " + ex.getMessage());
-        return ResponseEntity.badRequest().body("Email was not sent.");
+      log.error("Email was not sent there was an error" + ex.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There was a problem: " + ex.getMessage());
     }
   }
 }
